5dc5c972075f16d21b84362c5e623d4e78998e83
Nima Tsering Tamang <tamangtsering15@gmail.com>
2023-08-26 03:05:17 -0700

    add

--- src/Content.js
+++ src/Content.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const Content=()=>{
     return<div className="destination">
-        <h3>Supercharge Your Business Growth with Tanvi Hosting Services"
-</h3>
-        <p>The best domain names starting at only Rs. 366.58/yr*</p>
+        <h3>TanviTech Linux Web Hosting</h3>
+        <p>Truly your Hosting partner</p>
+        <div className="firs">
+            </div>
         </div>
 }
 
